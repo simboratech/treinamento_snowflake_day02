fb7d66eac2df640b33b0fe3ed7ac33d0719fdfbd
simboratech <122639098+simboratech@users.noreply.github.com>
2025-10-03 17:34:53 -0300

    Create warehouses.sql
    first warehouses

---
+++ 02 - warehouses/warehouses.sql
@@ -0,0 +1,42 @@
+/***************************************************************************************************       
+Asset:       Setup Warehouses to use
+Version:      v1     
+****************************************************************************************************/
+
+-- create warehouses
+CREATE OR REPLACE WAREHOUSE tb_de_wh
+    WAREHOUSE_SIZE = 'large' -- Large for initial data load - scaled down to XSmall at end of this scripts
+    WAREHOUSE_TYPE = 'standard'
+    AUTO_SUSPEND = 60
+    AUTO_RESUME = TRUE
+    INITIALLY_SUSPENDED = TRUE
+COMMENT = 'data engineering warehouse for tasty bytes';
+
+CREATE OR REPLACE WAREHOUSE tb_dev_wh
+    WAREHOUSE_SIZE = 'xsmall'
+    WAREHOUSE_TYPE = 'standard'
+    AUTO_SUSPEND = 60
+    AUTO_RESUME = TRUE
+    INITIALLY_SUSPENDED = TRUE
+COMMENT = 'developer warehouse for tasty bytes';
+
+-- create analyst warehouse
+CREATE OR REPLACE WAREHOUSE tb_analyst_wh
+    COMMENT = 'TastyBytes Analyst Warehouse'
+    WAREHOUSE_TYPE = 'standard'
+    WAREHOUSE_SIZE = 'large'
+    MIN_CLUSTER_COUNT = 1
+    MAX_CLUSTER_COUNT = 2
+    SCALING_POLICY = 'standard'
+    AUTO_SUSPEND = 60
+    INITIALLY_SUSPENDED = true,
+    AUTO_RESUME = true;
+
+-- Create a dedicated large warehouse for analytical workloads
+CREATE OR REPLACE WAREHOUSE tb_cortex_wh
+    WAREHOUSE_SIZE = 'LARGE'
+    WAREHOUSE_TYPE = 'STANDARD'
+    AUTO_SUSPEND = 60
+    AUTO_RESUME = TRUE
+    INITIALLY_SUSPENDED = TRUE
+COMMENT = 'Dedicated large warehouse for Cortex Analyst and other analytical tools.';
