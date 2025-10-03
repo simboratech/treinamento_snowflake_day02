f275bab1b15286d5be654a99dda372f6c5940663
simboratech <122639098+simboratech@users.noreply.github.com>
2025-10-03 17:59:34 -0300

    Create setup.sql
    first cortex

---
+++ 09 - cortex search/setup.sql
@@ -0,0 +1,21 @@
+-- Create or replace the Cortex Search Service named 'tasty_bytes_review_search'. --
+CREATE OR REPLACE CORTEX SEARCH SERVICE tb_101.harmonized.tasty_bytes_review_search
+ON REVIEW 
+ATTRIBUTES LANGUAGE, ORDER_ID, REVIEW_ID, TRUCK_BRAND_NAME, PRIMARY_CITY, DATE, SOURCE 
+WAREHOUSE = tb_de_wh
+TARGET_LAG = '1 hour' 
+AS (
+    SELECT
+        REVIEW,             
+        LANGUAGE,           
+        ORDER_ID,           
+        REVIEW_ID,          
+        TRUCK_BRAND_NAME,  
+        PRIMARY_CITY,       
+        DATE,               
+        SOURCE             
+    FROM
+        tb_101.harmonized.truck_reviews_v 
+    WHERE
+        REVIEW IS NOT NULL 
+);
