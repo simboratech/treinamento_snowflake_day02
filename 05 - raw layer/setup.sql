efe79591e3f5758e585801f9e8c2c9da8e59de94
simboratech <122639098+simboratech@users.noreply.github.com>
2025-10-03 17:54:45 -0300

    Create setup.sql
    first raw zone

---
+++ 05 - raw layer/setup.sql
@@ -0,0 +1,141 @@
+/*--
+ raw zone table build 
+--*/
+
+-- country table build
+CREATE OR REPLACE TABLE tb_101.raw_pos.country
+(
+    country_id NUMBER(18,0),
+    country VARCHAR(16777216),
+    iso_currency VARCHAR(3),
+    iso_country VARCHAR(2),
+    city_id NUMBER(19,0),
+    city VARCHAR(16777216),
+    city_population VARCHAR(16777216)
+);
+
+-- franchise table build
+CREATE OR REPLACE TABLE tb_101.raw_pos.franchise 
+(
+    franchise_id NUMBER(38,0),
+    first_name VARCHAR(16777216),
+    last_name VARCHAR(16777216),
+    city VARCHAR(16777216),
+    country VARCHAR(16777216),
+    e_mail VARCHAR(16777216),
+    phone_number VARCHAR(16777216) 
+);
+
+-- location table build
+CREATE OR REPLACE TABLE tb_101.raw_pos.location
+(
+    location_id NUMBER(19,0),
+    placekey VARCHAR(16777216),
+    location VARCHAR(16777216),
+    city VARCHAR(16777216),
+    region VARCHAR(16777216),
+    iso_country_code VARCHAR(16777216),
+    country VARCHAR(16777216)
+);
+
+-- menu table build
+CREATE OR REPLACE TABLE tb_101.raw_pos.menu
+(
+    menu_id NUMBER(19,0),
+    menu_type_id NUMBER(38,0),
+    menu_type VARCHAR(16777216),
+    truck_brand_name VARCHAR(16777216),
+    menu_item_id NUMBER(38,0),
+    menu_item_name VARCHAR(16777216),
+    item_category VARCHAR(16777216),
+    item_subcategory VARCHAR(16777216),
+    cost_of_goods_usd NUMBER(38,4),
+    sale_price_usd NUMBER(38,4),
+    menu_item_health_metrics_obj VARIANT
+);
+
+-- truck table build 
+CREATE OR REPLACE TABLE tb_101.raw_pos.truck
+(
+    truck_id NUMBER(38,0),
+    menu_type_id NUMBER(38,0),
+    primary_city VARCHAR(16777216),
+    region VARCHAR(16777216),
+    iso_region VARCHAR(16777216),
+    country VARCHAR(16777216),
+    iso_country_code VARCHAR(16777216),
+    franchise_flag NUMBER(38,0),
+    year NUMBER(38,0),
+    make VARCHAR(16777216),
+    model VARCHAR(16777216),
+    ev_flag NUMBER(38,0),
+    franchise_id NUMBER(38,0),
+    truck_opening_date DATE
+);
+
+-- order_header table build
+CREATE OR REPLACE TABLE tb_101.raw_pos.order_header
+(
+    order_id NUMBER(38,0),
+    truck_id NUMBER(38,0),
+    location_id FLOAT,
+    customer_id NUMBER(38,0),
+    discount_id VARCHAR(16777216),
+    shift_id NUMBER(38,0),
+    shift_start_time TIME(9),
+    shift_end_time TIME(9),
+    order_channel VARCHAR(16777216),
+    order_ts TIMESTAMP_NTZ(9),
+    served_ts VARCHAR(16777216),
+    order_currency VARCHAR(3),
+    order_amount NUMBER(38,4),
+    order_tax_amount VARCHAR(16777216),
+    order_discount_amount VARCHAR(16777216),
+    order_total NUMBER(38,4)
+);
+
+-- order_detail table build
+CREATE OR REPLACE TABLE tb_101.raw_pos.order_detail 
+(
+    order_detail_id NUMBER(38,0),
+    order_id NUMBER(38,0),
+    menu_item_id NUMBER(38,0),
+    discount_id VARCHAR(16777216),
+    line_number NUMBER(38,0),
+    quantity NUMBER(5,0),
+    unit_price NUMBER(38,4),
+    price NUMBER(38,4),
+    order_item_discount_amount VARCHAR(16777216)
+);
+
+-- customer loyalty table build
+CREATE OR REPLACE TABLE tb_101.raw_customer.customer_loyalty
+(
+    customer_id NUMBER(38,0),
+    first_name VARCHAR(16777216),
+    last_name VARCHAR(16777216),
+    city VARCHAR(16777216),
+    country VARCHAR(16777216),
+    postal_code VARCHAR(16777216),
+    preferred_language VARCHAR(16777216),
+    gender VARCHAR(16777216),
+    favourite_brand VARCHAR(16777216),
+    marital_status VARCHAR(16777216),
+    children_count VARCHAR(16777216),
+    sign_up_date DATE,
+    birthday_date DATE,
+    e_mail VARCHAR(16777216),
+    phone_number VARCHAR(16777216)
+);
+
+/*--
+ raw_suport zone table build 
+--*/
+CREATE OR REPLACE TABLE tb_101.raw_support.truck_reviews
+(
+    order_id NUMBER(38,0),
+    language VARCHAR(16777216),
+    source VARCHAR(16777216),
+    review VARCHAR(16777216),
+    review_id NUMBER(38,0)  
+);
