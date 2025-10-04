7ced8f41428780517becdc26ef4bc29f8b01f1e8
simboratech <122639098+simboratech@users.noreply.github.com>
2025-10-03 21:38:30 -0300

    Update setup.sql
    2 cortex

--- 09 - cortex search/setup.sql
+++ 09 - cortex search/setup.sql
@@ -1,9 +1,12 @@
+USE ROLE sysadmin;
+USE WAREHOUSE tb_de_wh;
+
 -- Create or replace the Cortex Search Service named 'tasty_bytes_review_search'. --
 CREATE OR REPLACE CORTEX SEARCH SERVICE tb_101.harmonized.tasty_bytes_review_search
 ON REVIEW 
 ATTRIBUTES LANGUAGE, ORDER_ID, REVIEW_ID, TRUCK_BRAND_NAME, PRIMARY_CITY, DATE, SOURCE 
 WAREHOUSE = tb_de_wh
-TARGET_LAG = '1 hour' 
+TARGET_LAG = '1 day' 
 AS (
     SELECT
         REVIEW,             
